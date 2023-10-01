03600007bdd4b9369c412f93f63c415124a93863
Dongyue Zhang <zhangdongyue22@gmail.com>
2023-09-30 22:27:30 -0400

    Applied MVC pattern

---
+++ TravelExpenseDAO.cpp
@@ -0,0 +1,8 @@
+//
+//  TravelExpenseDAO.cpp
+//  ProgrammingLanguageResearchProject
+//
+//  Created by Dongyue Zhang on 2023-09-27.
+//
+
+#include "TravelExpenseDAO.hpp"
---
+++ TravelExpenseDAO.hpp
@@ -0,0 +1,13 @@
+//
+//  TravelExpenseDAO.hpp
+//  ProgrammingLanguageResearchProject
+//
+//  Created by Dongyue Zhang on 2023-09-27.
+//
+
+#ifndef TravelExpenseDAO_hpp
+#define TravelExpenseDAO_hpp
+
+#include <stdio.h>
+
+#endif /* TravelExpenseDAO_hpp */
